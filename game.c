ecc50b57652d3109c411be4a23a94af398bc40e8
averagestd <112917081+averagestd@users.noreply.github.com>
2022-10-20 14:01:35 +0800

    Add files via upload

--- game.c
+++ game.c
@@ -1,19 +1,18 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include "game.h"
-void menu()
+
+void Alter()
 {
 	printf("***************************************\n");
 	printf("***************************************\n");
-	printf("************    1.begin    ************\n");
+	printf("************    1.start    ************\n");
 	printf("************    0.leave    ************\n");
 	printf("***************************************\n");
 	printf("***************************************\n");
-
+	printf("please input:>");
 }
 
-
-
-void Distri_Board(int boar[ROW][COL], int row, int col, int rowor, int color)
+void Init_quar(char place[ROW][COL], int row, int col)
 {
 	int i = 0;
 	for (i = 0; i < row; i++)
@@ -21,60 +20,12 @@ void Distri_Board(int boar[ROW][COL], int row, int col, int rowor, int color)
 		int j = 0;
 		for (j = 0; j < col; j++)
 		{
-			boar[i][j] = 0;
-		}
-
-		//随机个数，随机分配
-
-		//for (j = 0; j < col; j++)
-		//{
-		//	int ranm = rand();
-		//	if (ranm % 7 == 0||ranm % 3 == 0)
-		//	{
-		//		boar[i][j] = ranm % 2;
-		//	}
-		//	else
-		//	{
-		//		boar[i][j] = 0;
-		//	}
-		//	if (j == 0 || j == col - 1)
-		//	{
-		//		boar[i][j] = 0;
-		//	}
-		//}
-		//if (i == 0 || i == row - 1)
-		//{
-		//	for (j = 0; j < col; j++)
-		//	{
-		//		boar[i][j] = 0;
-		//	}
-		//}
-	}
-	int count = EasyP;
-	while (count > 0)
-	{
-		int x = rand() % rowor + 1;
-		int y = rand() % color + 1;
-		if (boar[x][y] == 0)
-		{
-			boar[x][y] = 1;
-			count--;
+			place[i][j] = ' ';
 		}
 	}
-
-	////测试雷分布
-	//for (i = 0; i < row; i++)
-	//{
-	//	int j = 0;
-	//	for (j = 0; j < col; j++)
-	//	{
-	//		printf("%d ", boar[i][j]);
-	//	}
-	//	printf("\n");
-	//}
 }
 
-void Init_Board(char boar[ROW_OR][COL_OR], int row, int col)
+void Display_Board(char place[ROW][COL], int row, int col)
 {
 	int i = 0;
 	for (i = 0; i < row; i++)
@@ -82,109 +33,130 @@ void Init_Board(char boar[ROW_OR][COL_OR], int row, int col)
 		int j = 0;
 		for (j = 0; j < col; j++)
 		{
-			boar[i][j] = '*';
-		}
-	}
-}
-	
-
-
-void Display_Board(char boar[ROW_OR][COL_OR], int row, int col)
-{
-	printf("----------- 扫雷游戏 -----------\n");
-	printf("0  1  2  3  4  5  6  7  8  9\n\n");
-	int i = 0;
-	for (i = 0; i < row; i++)
-	{
-		int j = 0;
-		printf("%d  ", i + 1);
-		for (j = 0; j < col; j++)
-		{
-			printf("%c  ", boar[i][j]);
-		}
-		printf("\n\n");
-	}
-	printf("----------- 扫雷游戏 -----------\n");
-}
-
-//此函数功能不够单一，如要更改比较困难，往后需注意此问题
-
-int Player_Discov(int boar[ROW][COL], char obser_boar[ROW_OR][COL_OR], int row, int col, int i, int j)
-{
-
-	if ((obser_boar[i - 1][j - 1] == '*') && (i > 0 && i < 10 && j>0 && j < 10))
-	{
-		if (boar[i][j])
-		{
-			obser_boar[i - 1][j - 1] = '!';
-			printf("你被炸死了！\n");
-			return 0;
-		}
-		if (boar[i][j] == 0)
-		{
-			int sum = 0;
-			int m = 0;
-			for (m = i - 1; m < i + 2; m++)
+			printf(" %c ", place[i][j]);
+			if (j < col - 1)
 			{
-				int n = 0;
-				for (n = j - 1; n < j + 2; n++)
+				printf("|");
+			}
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
 				{
-					sum += boar[m][n];
+					printf("|");
 				}
 			}
-			obser_boar[i - 1][j - 1] = (char)(sum + '0');
-			if (sum == 0)
+			printf("\n");
+		}
+	}
+}
+
+void Player_Procd(char place[ROW][COL], int row, int col)
+{
+	int input1 = 0;
+	int input2 = 0;
+	while (1)
+	{
+		printf("player's round, put on:");
+		scanf("%d %d", &input1, &input2);
+		if (input1 > 0 && input1 <= 3 && input2 > 0 && input2 <= 3)
+		{
+			if (place[input1 - 1][input2 - 1] == ' ')
 			{
-				int t = 0;
-				for (t = i - 1; t < i + 2; t++)
+				place[input1 - 1][input2 - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("this blank has been occupied, input again\n");
+			}
+		}
+		else
+		{
+			printf("error, input again\n");
+		}
+	}
+}
+
+void AI_Procd(char place[ROW][COL], int row, int col)
+{
+	printf("AI's round\n");
+	while (1)
+	{
+		int x = rand() % ROW;
+		int y = rand() % COL;
+		int i = 0;
+		//智能拦截实现
+		for (i = 0; i < row; i++)
+		{
+			if (((place[i][0] == place[i][1] || place[i][1] == place[i][2]) && place[i][1] != ' '))
+			{
+				int c = 0;
+				for (c = 0; c < col; c++)
 				{
-					int s = 0;
-					for (s = j - 1; s < j + 2; s++)
+					if (place[i][c] == ' ')
 					{
-						Player_Discov(boar, obser_boar, ROW, COL, t, s);
+						place[i][c] = '#';
+						goto end;
 					}
 				}
 			}
-			
-			return 1;
+			else if((place[i][0] == place[i][2]) && place[i][0] != ' ' && place[i][1] == ' ')
+			{
+				place[i][1] = '#';
+				goto end;
+			}
 		}
-	}
-	else
-	{
-		return 1;
-	}
+		if (place[x][y] == ' ')
+		{
+			place[x][y] = '#';
+			goto end;
+		}
 
+	}
+	end:
+		;
 }
 
-int Judge_win(int boar[ROW][COL], char obser_boar[ROW_OR][COL_OR], int row, int col)
+char Judg(char place[ROW][COL], int row, int col)
 {
 	int i = 0;
-	int count1 = 0;
-	int count2 = 0;
+	char flag = '0';
 	for (i = 0; i < row; i++)
 	{
+		if (place[i][0] == place[i][1] && place[i][1] == place[i][2] && place[i][0] != ' ')
+		{
+			return place[i][0];
+		}
 		int j = 0;
 		for (j = 0; j < col; j++)
 		{
-			if (boar[i+1][j+1] == 1)
+			//横三行判断
+			
+			//竖三行判断
+			if (place[0][j] == place[1][j] && place[1][j] == place[2][j] && place[0][j] != ' ')
 			{
-				count1++;
+				return place[0][j];
 			}
-			if (obser_boar[i][j] == '*')
+			//斜两行判断
+			if (((place[0][0] == place[1][1] && place[1][1] == place[2][2]) || (place[0][2] == place[1][1] && place[1][1] == place[2][0])) && place[1][1] != ' ')
 			{
-				count2++;
+				return place[1][1];
 			}
+			if (place[i][j] == ' ')
+				flag = '1';
 		}
+
 	}
-	if (count1 == count2)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+	return flag;
+
 }
+
+
 
 
 
